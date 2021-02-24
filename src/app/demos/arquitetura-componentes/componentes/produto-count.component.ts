8b59fb72e352df6e8482ef87ccd872392056da5a
Guilherme Vada <gvadaguariba@gmail.com>
2021-02-24 20:50:13 -0300

    created component product details and product count

---
+++ src/app/demos/arquitetura-componentes/componentes/produto-count.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { Produto } from '../models/produto';
+
+@Component({
+    selector: 'produto-count',
+    templateUrl: './produto-count.component.html'
+})
+
+export class ProdutoCountComponent {
+    @Input()
+    produtos: Produto[];
+
+    contadorAtivos(): number {
+        if (!this.produtos) return;
+
+        return this.produtos.filter((produto: Produto) => produto.ativo).length;
+    }
+}
