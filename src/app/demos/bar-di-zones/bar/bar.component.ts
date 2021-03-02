5b759287ea8fb4a2aa80ae77e63c5bd0b393a61c
Guilherme Vada <gvadaguariba@gmail.com>
2021-03-02 18:43:19 -0300

    Understand UseFactory

--- src/app/demos/bar-di-zones/bar/bar.component.ts
+++ src/app/demos/bar-di-zones/bar/bar.component.ts
@@ -1,19 +1,22 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject, Injector, OnInit } from '@angular/core';
 import { BarUnidadeConfig, BAR_UNIDADE_CONFIG } from './bar.config';
-import { BarServices, BarServicesMock } from './bar.service';
+import { BarFactory, BarServices, BarServicesMock } from './bar.service';
 
 @Component({
   selector: 'app-bar',
   templateUrl: './bar.component.html',
-  providers: [{
-    provide: BarServices, useClass: BarServices //BarServicesMock
-  }]
+  providers: [
+    // { provide: BarServices, useClass: BarServices },
+    { provide: BarServices, useFactory: BarFactory, deps: [HttpClient, Injector] }
+  ]
 })
 
 export class BarComponent implements OnInit {
 
   barBebida1: string;
   configManual: BarUnidadeConfig;
+  dadosUnidade: string;
 
   constructor(
     private barServices: BarServices,
@@ -22,5 +25,6 @@ export class BarComponent implements OnInit {
   ngOnInit() {
     this.barBebida1 = this.barServices.obterBebidas();
     this.configManual = this.ApiConfigManual;
+    this.dadosUnidade = this.barServices.obterUnidade();
   }
 }
--- src/app/demos/bar-di-zones/bar/bar.module.ts
+++ src/app/demos/bar-di-zones/bar/bar.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
 import { ModuleWithProviders, NgModule } from "@angular/core";
 import { BarComponent } from "./bar.component";
 import { BarUnidadeConfig, BAR_UNIDADE_CONFIG } from "./bar.config";
 
 @NgModule({
     imports: [
-        CommonModule
+        CommonModule,
+        HttpClientModule
     ],
     declarations: [
         BarComponent
