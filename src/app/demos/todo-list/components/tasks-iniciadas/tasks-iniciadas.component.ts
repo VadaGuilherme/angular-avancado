f74c43e9e9cf28c2faeb29b5295b7c2b7f3e2b94
Guilherme Vada <gvadaguariba@gmail.com>
2021-03-04 21:30:22 -0300

    Implementation store in session todo

--- src/app/demos/todo-list/components/tasks-iniciadas/tasks-iniciadas.component.ts
+++ src/app/demos/todo-list/components/tasks-iniciadas/tasks-iniciadas.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { TasksService } from '../../todo.service';
+import { Store } from '../../todo.store';
 
 @Component({
   selector: 'tasks-iniciadas',
@@ -11,10 +13,13 @@ export class TasksIniciadasComponent implements OnInit {
 
   iniciados$: Observable<any[]>;
 
-  constructor(private tasksService: TasksService) { }
+  constructor(private taskService: TasksService, private store: Store) { }
 
   ngOnInit() {
-    this.iniciados$ = this.tasksService.getTodoList$;
+    this.iniciados$ = this.store.getTodoList().pipe(map(todolist => todolist.filter(task => task.iniciado && !task.finalizado)));
   }
 
+  onToggle(event: any) {
+    this.taskService.toggle(event);
+  }
 }
--- src/app/demos/todo-list/components/tasks/tasks.component.ts
+++ src/app/demos/todo-list/components/tasks/tasks.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { TasksService } from '../../todo.service';
+import { Store } from '../../todo.store';
 
 @Component({
   selector: 'tasks',
   templateUrl: './tasks.component.html',
   styles: []
 })
-export class TasksComponent implements OnInit {
+export class TasksComponent implements OnInit, OnDestroy {
 
   todoList$: Observable<any[]>;
+  subscription: Subscription;
 
-  constructor(private taskService: TasksService) { }
-
+  constructor(private taskService: TasksService, private store: Store) { }
   ngOnInit() {
-    this.todoList$ = this.taskService.getTodoList$;
+    this.todoList$ = this.store.getTodoList().pipe(map(todolist => todolist.filter(task => !task.iniciado && !task.finalizado)));
+
+    this.subscription = this.taskService.getTodoList$.subscribe();
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  onToggle(event: any) {
+    this.taskService.toggle(event);
+  }
 }
